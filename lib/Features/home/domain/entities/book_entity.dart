7919fe4421ce5e7e717043bea59d57b2f223c085
ahmad <alsakniahmad51@gmail.com>
2023-10-31 13:04:40 +0300

    home repo

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authername;
+  final num price;
+  final num raiting;
+
+  BookEntity(this.image, this.title, this.authername, this.price, this.raiting);
+}
